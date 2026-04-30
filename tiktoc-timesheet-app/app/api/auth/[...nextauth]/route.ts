ef56e4bc56dcc5ff3b0701504bb4a06f664f50f8
shahzebrehmankhattak <shahzebkhattak703@gmail.com>
2026-05-01 00:43:50 +0500

    env changed

--- tiktoc-timesheet-app/app/api/auth/[...nextauth]/route.ts
+++ tiktoc-timesheet-app/app/api/auth/[...nextauth]/route.ts
@@ -23,6 +23,7 @@ const handler = NextAuth({
   session: {
     strategy: "jwt",
   },
+  secret: process.env.NEXTAUTH_SECRET, // 👈 THIS IS REQUIRED
 });
 
 export { handler as GET, handler as POST };
